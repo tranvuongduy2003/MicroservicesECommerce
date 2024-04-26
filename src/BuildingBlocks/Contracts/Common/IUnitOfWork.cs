8a616ba17a43f07a43016cc77a99cbed5f33d425
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2024-04-27 04:10:17 +0700

    Initialize Entities, Dtos, Repository Pattern for Product.API

---
+++ src/BuildingBlocks/Contracts/Common/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Contracts.Common
+{
+    public interface IUnitOfWork<TContext> : IDisposable where TContext : DbContext
+    {
+        Task<int> CommitAsync();
+    }
+}
---
+++ src/BuildingBlocks/Contracts/Domains/EntityAuditBase.cs
@@ -0,0 +1,11 @@
+﻿using Contracts.Domains.Interfaces;
+
+namespace Contracts.Domains
+{
+    public class EntityAuditBase<T> : EntityBase<T>, IAuditable
+    {
+        public DateTimeOffset CreatedDate { get; set; }
+
+        public DateTimeOffset? LastModifiedDate { get; set; }
+    }
+}
