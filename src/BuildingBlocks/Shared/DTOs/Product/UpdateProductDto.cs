527f07fef6532cb295e08d401aed5549da7613e5
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2024-04-27 17:21:10 +0700

    CRUD for Product.API

---
+++ src/BuildingBlocks/Shared/DTOs/Product/UpdateProductDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared.DTOs.Product
+{
+    public class UpdateProductDto
+    {
+        public string Name { get; set; }
+
+        public string Summary { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
